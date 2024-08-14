8f41eb2d7a3689e513ad603a98ee36cb5bc8cca0
tuguzT <timurka.tugushev@gmail.com>
2024-08-14 15:54:37 +0300

    Add entry for arena

--- crates/gpecs_sparse/src/arena.rs
+++ crates/gpecs_sparse/src/arena.rs
@@ -1,7 +1,7 @@
 use alloc::{boxed::Box, collections::TryReserveError, vec::Vec};
 use core::{
     cmp,
-    fmt::Display,
+    fmt::{self, Debug, Display},
     mem::{replace, swap},
     ops::{Index, IndexMut},
 };
@@ -990,9 +990,29 @@ where
         true
     }
 
-    pub fn entry(&mut self, key: K) {
-        let _ = key;
-        todo!()
+    pub fn entry(&mut self, key: K) -> Entry<'_, K, V> {
+        let Self {
+            dense_keys, sparse, ..
+        } = self;
+
+        let sparse_index = key.sparse_index();
+        let Some(dense_index) = sparse
+            .get(sparse_index)
+            .take_if(|item| item.epoch == key.epoch())
+            .and_then(SparseItem::dense_index)
+        else {
+            let sparse_set = self;
+            let entry = VacantEntry { key, sparse_set };
+            return Entry::Vacant(entry);
+        };
+
+        check_dense_index_bounds(dense_index, dense_keys.len());
+        let entry = OccupiedEntry {
+            key,
+            dense_index,
+            sparse_set: self,
+        };
+        Entry::Occupied(entry)
     }
 
     pub fn clear(&mut self) {
@@ -1273,6 +1293,309 @@ where
             self.push(value);
         }
     }
+}
+
+#[derive(Debug)]
+pub enum Entry<'a, K, V>
+where
+    K: Key,
+{
+    Occupied(OccupiedEntry<'a, K, V>),
+    Vacant(VacantEntry<'a, K, V>),
+}
+
+impl<'a, K, V> Entry<'a, K, V>
+where
+    K: Key,
+{
+    #[inline]
+    pub const fn is_occupied(&self) -> bool {
+        matches!(self, Self::Occupied(_))
+    }
+
+    #[inline]
+    pub const fn is_vacant(&self) -> bool {
+        matches!(self, Self::Vacant(_))
+    }
+
+    #[inline]
+    pub fn key(&self) -> K {
+        match self {
+            Self::Occupied(entry) => entry.key(),
+            Self::Vacant(entry) => entry.key(),
+        }
+    }
+
+    #[inline]
+    pub fn get(&self) -> Option<&V> {
+        match self {
+            Self::Occupied(entry) => Some(entry.get()),
+            Self::Vacant(_) => None,
+        }
+    }
+
+    #[inline]
+    pub fn get_mut(&mut self) -> Option<&mut V> {
+        match self {
+            Self::Occupied(entry) => Some(entry.get_mut()),
+            Self::Vacant(_) => None,
+        }
+    }
+
+    #[inline]
+    pub fn and_modify<F>(self, f: F) -> Self
+    where
+        F: FnOnce(&mut V),
+    {
+        match self {
+            Self::Occupied(mut entry) => {
+                f(entry.get_mut());
+                Self::Occupied(entry)
+            }
+            Self::Vacant(entry) => Self::Vacant(entry),
+        }
+    }
+
+    #[inline]
+    pub fn or_insert(self, default: V) -> &'a mut V {
+        match self {
+            Self::Occupied(entry) => entry.into_mut(),
+            Self::Vacant(entry) => entry.insert(default),
+        }
+    }
+
+    #[inline]
+    pub fn or_insert_with<F>(self, default: F) -> &'a mut V
+    where
+        F: FnOnce() -> V,
+    {
+        match self {
+            Self::Occupied(entry) => entry.into_mut(),
+            Self::Vacant(entry) => entry.insert(default()),
+        }
+    }
+
+    #[inline]
+    pub fn or_default(self) -> &'a mut V
+    where
+        V: Default,
+    {
+        match self {
+            Self::Occupied(entry) => entry.into_mut(),
+            Self::Vacant(entry) => entry.insert(Default::default()),
+        }
+    }
+
+    #[inline]
+    pub fn insert_entry(self, value: V) -> OccupiedEntry<'a, K, V> {
+        match self {
+            Self::Occupied(mut entry) => {
+                entry.insert(value);
+                entry
+            }
+            Self::Vacant(entry) => entry.insert_entry(value),
+        }
+    }
+
+    #[inline]
+    pub fn replace_key(self, key: K) -> Self {
+        match self {
+            Self::Occupied(mut entry) => {
+                entry.replace_key(key);
+                Self::Occupied(entry)
+            }
+            Self::Vacant(entry) => {
+                let VacantEntry { sparse_set, .. } = entry;
+                sparse_set.entry(key)
+            }
+        }
+    }
+}
+
+pub struct OccupiedEntry<'a, K, V>
+where
+    K: Key,
+{
+    key: K,
+    dense_index: usize,
+    sparse_set: &'a mut EpochSparseArena<K, V>,
+}
+
+impl<'a, K, V> OccupiedEntry<'a, K, V>
+where
+    K: Key,
+{
+    #[inline]
+    pub fn get(&self) -> &V {
+        let Self {
+            dense_index,
+            sparse_set,
+            ..
+        } = self;
+
+        let values = sparse_set.dense_values.as_slice();
+        unwrap_dense_value(values, *dense_index)
+    }
+
+    #[inline]
+    pub fn get_mut(&mut self) -> &mut V {
+        let Self {
+            dense_index,
+            sparse_set,
+            ..
+        } = self;
+
+        let values = sparse_set.dense_values.as_mut_slice();
+        unwrap_dense_value_mut(values, *dense_index)
+    }
+
+    #[inline]
+    pub fn into_mut(self) -> &'a mut V {
+        let Self {
+            dense_index,
+            sparse_set,
+            ..
+        } = self;
+
+        let values = sparse_set.dense_values.as_mut_slice();
+        unwrap_dense_value_mut(values, dense_index)
+    }
+
+    #[inline]
+    pub fn insert(&mut self, value: V) -> V {
+        let previous = self.get_mut();
+        replace(previous, value)
+    }
+}
+
+impl<'a, K, V> OccupiedEntry<'a, K, V>
+where
+    K: Key,
+{
+    #[inline]
+    pub fn key(&self) -> K {
+        let Self { key, .. } = self;
+        *key
+    }
+
+    #[inline]
+    pub fn remove(self) -> V {
+        let Self {
+            key, sparse_set, ..
+        } = self;
+
+        let value = sparse_set.remove(key);
+        unwrap_sparse_value(value)
+    }
+
+    #[inline]
+    pub fn swap_remove(self) -> V {
+        let Self {
+            key, sparse_set, ..
+        } = self;
+
+        let value = sparse_set.swap_remove(key);
+        unwrap_sparse_value(value)
+    }
+
+    #[inline]
+    pub fn replace_key(&mut self, key: K) -> Option<V> {
+        let new_key = key;
+        let Self {
+            key, sparse_set, ..
+        } = self;
+
+        let value = sparse_set.remove(*key);
+        let value = unwrap_sparse_value(value);
+
+        *key = new_key;
+        sparse_set.insert(*key, value)
+    }
+}
+
+impl<'a, K, V> Debug for OccupiedEntry<'a, K, V>
+where
+    K: Key + Debug,
+    V: Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Self { key, .. } = self;
+
+        let value = self.get();
+        f.debug_struct("OccupiedEntry")
+            .field("key", key)
+            .field("value", value)
+            .finish()
+    }
+}
+
+pub struct VacantEntry<'a, K, V>
+where
+    K: Key,
+{
+    key: K,
+    sparse_set: &'a mut EpochSparseArena<K, V>,
+}
+
+impl<'a, K, V> VacantEntry<'a, K, V>
+where
+    K: Key,
+{
+    #[inline]
+    pub fn key(&self) -> K {
+        let Self { key, .. } = self;
+        *key
+    }
+
+    #[inline]
+    pub fn insert(self, value: V) -> &'a mut V {
+        let Self { key, sparse_set } = self;
+
+        sparse_set.insert(key, value);
+
+        let value = sparse_set.dense_values.last_mut();
+        unwrap_sparse_value(value)
+    }
+
+    #[inline]
+    pub fn insert_entry(self, value: V) -> OccupiedEntry<'a, K, V> {
+        let Self { key, sparse_set } = self;
+
+        sparse_set.insert(key, value);
+        let dense_index = sparse_set.dense_values.len() - 1;
+
+        OccupiedEntry {
+            key,
+            dense_index,
+            sparse_set,
+        }
+    }
+}
+
+impl<'a, K, V> Debug for VacantEntry<'a, K, V>
+where
+    K: Key + Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Self { key, .. } = self;
+        f.debug_struct("VacantEntry").field("key", key).finish()
+    }
+}
+
+#[cold]
+#[track_caller]
+#[inline(never)]
+const fn unwrap_sparse_value_failed() -> ! {
+    panic!("value by provided key should exist")
+}
+
+#[inline]
+#[track_caller]
+fn unwrap_sparse_value<T>(value: Option<T>) -> T {
+    let Some(value) = value else {
+        unwrap_sparse_value_failed()
+    };
+    value
 }
 
 #[cfg(test)]
@@ -2630,30 +2953,30 @@ mod tests {
         assert_eq!(sparse_arena.get(6), Some(&34));
     }
 
-    // #[test]
-    // fn five_items_entry() {
-    //     let mut sparse_arena = SparseArena::new();
-    //     sparse_arena.insert(8, 42);
-    //     sparse_arena.insert(1, 228);
-    //     sparse_arena.insert(4, 69);
-    //     sparse_arena.insert(3, 666);
-    //     sparse_arena.insert(6, 34);
+    #[test]
+    fn five_items_entry() {
+        let mut sparse_arena = SparseArena::new();
+        sparse_arena.insert(8, 42);
+        sparse_arena.insert(1, 228);
+        sparse_arena.insert(4, 69);
+        sparse_arena.insert(3, 666);
+        sparse_arena.insert(6, 34);
 
-    //     let entry = sparse_arena.entry(0);
-    //     assert_eq!(entry.key(), 0);
-    //     assert_eq!(entry.get(), None);
+        let entry = sparse_arena.entry(0);
+        assert_eq!(entry.key(), 0);
+        assert_eq!(entry.get(), None);
 
-    //     let entry = entry.and_modify(|value| *value += 1);
-    //     assert_eq!(entry.key(), 0);
-    //     assert_eq!(entry.get(), None);
+        let entry = entry.and_modify(|value| *value += 1);
+        assert_eq!(entry.key(), 0);
+        assert_eq!(entry.get(), None);
 
-    //     let entry = entry.replace_key(1);
-    //     assert_eq!(entry.key(), 1);
-    //     assert_eq!(entry.get(), Some(&228));
+        let entry = entry.replace_key(1);
+        assert_eq!(entry.key(), 1);
+        assert_eq!(entry.get(), Some(&228));
 
-    //     let value = entry.and_modify(|value| *value += 1).or_insert(47);
-    //     assert_eq!(value, &229);
-    // }
+        let value = entry.and_modify(|value| *value += 1).or_insert(47);
+        assert_eq!(value, &229);
+    }
 
     #[test]
     fn from_keys_values_iter() {

785472b3d2c8b6b4cc5f986e3861a332bf4f6556
tuguzT <timurka.tugushev@gmail.com>
2024-08-24 18:29:46 +0300

    Implement `reserve`, `shrink` and `truncate` methods

--- crates/gpecs_soa/src/vec.rs
+++ crates/gpecs_soa/src/vec.rs
@@ -1,11 +1,12 @@
-use alloc::{boxed::Box, collections::TryReserveError, vec::Vec};
+use alloc::{collections::TryReserveError, vec::Vec};
 use core::{
     marker::PhantomData,
     ops::{Deref, DerefMut},
+    ptr,
 };
 
 use crate::{
-    multi_vec_len_in_bytes,
+    multi_vec_len_in_bytes, multi_vec_ptrs,
     slice::{from_raw_parts, from_raw_parts_mut, MultiSlice},
 };
 
@@ -62,6 +63,38 @@ impl<T, U, V> MultiVec<T, U, V> {
             .unwrap_or(usize::MAX)
     }
 
+    fn move_right(&mut self, old_capacity: usize) {
+        let new_capacity = self.capacity();
+        if new_capacity <= old_capacity {
+            return;
+        }
+
+        unsafe {
+            let old_ptrs = multi_vec_ptrs::<T, U, V>(self.as_mut_ptr(), old_capacity);
+            let new_ptrs = multi_vec_ptrs::<T, U, V>(self.as_mut_ptr(), new_capacity);
+
+            ptr::copy(old_ptrs.v_ptr, new_ptrs.v_ptr, self.len());
+            ptr::copy(old_ptrs.u_ptr, new_ptrs.u_ptr, self.len());
+            ptr::copy(old_ptrs.t_ptr, new_ptrs.t_ptr, self.len());
+        }
+    }
+
+    fn move_left(&mut self, new_capacity: usize) {
+        let old_capacity = self.capacity();
+        if new_capacity >= old_capacity {
+            return;
+        }
+
+        unsafe {
+            let old_ptrs = multi_vec_ptrs::<T, U, V>(self.as_mut_ptr(), old_capacity);
+            let new_ptrs = multi_vec_ptrs::<T, U, V>(self.as_mut_ptr(), new_capacity);
+
+            ptr::copy(old_ptrs.t_ptr, new_ptrs.t_ptr, self.len());
+            ptr::copy(old_ptrs.u_ptr, new_ptrs.u_ptr, self.len());
+            ptr::copy(old_ptrs.v_ptr, new_ptrs.v_ptr, self.len());
+        }
+    }
+
     pub fn reserve(&mut self, additional: usize) {
         let old_capacity = self.capacity();
         if old_capacity == 0 {
@@ -72,69 +105,100 @@ impl<T, U, V> MultiVec<T, U, V> {
 
         let additional = additional * (size_of::<T>() + size_of::<U>() + size_of::<V>());
         self.inner.reserve(additional);
-        if old_capacity == self.capacity() {
-            return;
-        }
-
-        todo!("move data to the right starting from the end")
+        self.move_right(old_capacity);
     }
 
     pub fn reserve_exact(&mut self, additional: usize) {
-        let size_of_all = size_of::<T>() + size_of::<U>() + size_of::<V>();
-        self.inner.reserve_exact(additional * size_of_all);
+        let old_capacity = self.capacity();
+        if old_capacity == 0 {
+            let additional = multi_vec_len_in_bytes::<T, U, V>(additional);
+            self.inner.reserve_exact(additional);
+            return;
+        }
 
-        // TODO move data if needed
+        let additional = additional * (size_of::<T>() + size_of::<U>() + size_of::<V>());
+        self.inner.reserve_exact(additional);
+        self.move_right(old_capacity);
     }
 
     pub fn try_reserve(&mut self, additional: usize) -> Result<(), TryReserveError> {
-        let size_of_all = size_of::<T>() + size_of::<U>() + size_of::<V>();
-        self.inner.try_reserve(additional * size_of_all)?;
+        let old_capacity = self.capacity();
+        if old_capacity == 0 {
+            let additional = multi_vec_len_in_bytes::<T, U, V>(additional);
+            return self.inner.try_reserve(additional);
+        }
 
-        // TODO move data if needed
+        let additional = additional * (size_of::<T>() + size_of::<U>() + size_of::<V>());
+        self.inner.try_reserve(additional)?;
+        self.move_right(old_capacity);
         Ok(())
     }
 
     pub fn try_reserve_exact(&mut self, additional: usize) -> Result<(), TryReserveError> {
-        let size_of_all = size_of::<T>() + size_of::<U>() + size_of::<V>();
-        self.inner.try_reserve_exact(additional * size_of_all)?;
+        let old_capacity = self.capacity();
+        if old_capacity == 0 {
+            let additional = multi_vec_len_in_bytes::<T, U, V>(additional);
+            return self.inner.try_reserve_exact(additional);
+        }
 
-        // TODO move data if needed
+        let additional = additional * (size_of::<T>() + size_of::<U>() + size_of::<V>());
+        self.inner.try_reserve_exact(additional)?;
+        self.move_right(old_capacity);
         Ok(())
     }
 
     pub fn shrink_to_fit(&mut self) {
-        // TODO move data if needed
+        if self.capacity() == self.len() {
+            return;
+        }
+
+        let len = self.len();
+        let new_byte_capacity = multi_vec_len_in_bytes::<T, U, V>(len);
+        self.move_left(len);
 
         unsafe {
-            let old_len = self.inner.len();
-            let new_len = self.inner.capacity();
-
-            self.inner.set_len(new_len);
+            self.inner.set_len(new_byte_capacity);
             self.inner.shrink_to_fit();
-            self.inner.set_len(old_len);
+            self.inner.set_len(len);
         }
     }
 
     pub fn shrink_to(&mut self, min_capacity: usize) {
-        // TODO move data if needed
+        if self.capacity() <= min_capacity {
+            return;
+        }
+
+        let len = self.len();
+        let new_byte_capacity = multi_vec_len_in_bytes::<T, U, V>(min_capacity);
+        self.move_left(min_capacity);
 
         unsafe {
-            let old_len = self.inner.len();
-            let new_len = self.inner.capacity().min(min_capacity);
-
-            self.inner.set_len(new_len);
-            self.inner.shrink_to(min_capacity);
-            self.inner.set_len(old_len);
+            self.inner.set_len(new_byte_capacity);
+            self.inner.shrink_to_fit();
+            self.inner.set_len(len);
         }
     }
 
-    pub fn into_boxed_slice(self) -> Box<MultiSlice<T, U, V>> {
-        todo!()
-    }
-
     pub fn truncate(&mut self, len: usize) {
-        let _ = len;
-        todo!()
+        let new_len = len;
+        let old_len = self.len();
+        if new_len > old_len {
+            return;
+        }
+
+        let remaining_len = old_len - new_len;
+        unsafe {
+            let (t_ptr, u_ptr, v_ptr) = self.as_mut_ptrs();
+            let t_slice = ptr::slice_from_raw_parts_mut(t_ptr.add(new_len), remaining_len);
+            let u_slice = ptr::slice_from_raw_parts_mut(u_ptr.add(new_len), remaining_len);
+            let v_slice = ptr::slice_from_raw_parts_mut(v_ptr.add(new_len), remaining_len);
+
+            self.inner.set_len(new_len);
+
+            ptr::drop_in_place(t_slice);
+            ptr::drop_in_place(u_slice);
+            ptr::drop_in_place(v_slice);
+        }
     }
 
     pub fn as_slice(&self) -> &MultiSlice<T, U, V> {

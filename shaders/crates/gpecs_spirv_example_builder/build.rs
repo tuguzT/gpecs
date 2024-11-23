53be9f3668d80837101e2f2a76f3eaa473e01a9b
tuguzT <timurka.tugushev@gmail.com>
2024-11-23 23:44:19 +0300

    More metadata

--- shaders/crates/gpecs_spirv_example_builder/build.rs
+++ shaders/crates/gpecs_spirv_example_builder/build.rs
@@ -1,10 +1,11 @@
 use std::error::Error;
 
-use spirv_builder::{MetadataPrintout, ShaderPanicStrategy, SpirvBuilder};
+use spirv_builder::{MetadataPrintout, ShaderPanicStrategy, SpirvBuilder, SpirvMetadata};
 
 fn main() -> Result<(), Box<dyn Error>> {
     SpirvBuilder::new("../gpecs_spirv_example", "spirv-unknown-spv1.3")
         .print_metadata(MetadataPrintout::Full)
+        .spirv_metadata(SpirvMetadata::Full)
         .extension("SPV_KHR_non_semantic_info")
         .shader_panic_strategy(ShaderPanicStrategy::DebugPrintfThenExit {
             print_inputs: true,

0641d85d62b705f6d200dea2bd571f515c1cf706
tuguzT <timurka.tugushev@gmail.com>
2024-11-23 18:09:19 +0300

    Slices retrieved as storage buffers and debugPrintf are working

--- shaders/crates/gpecs_spirv_example/src/lib.rs
+++ shaders/crates/gpecs_spirv_example/src/lib.rs
@@ -1,15 +1,32 @@
 #![cfg_attr(target_arch = "spirv", no_std)]
 #![allow(unexpected_cfgs)] // to not to warn about `spirv` target arch
 #![deny(warnings)] // to see warnings from `spirv-builder` builds
+#![feature(asm_experimental_arch)]
 
-use spirv_std::{glam::UVec3, spirv};
+use spirv_std::{glam::UVec3, macros::debug_printfln, spirv};
+
+#[allow(dead_code)]
+struct SliceTest<'a, T, U, V> {
+    t_slice: &'a mut [T],
+    u_slice: &'a [U],
+    v_slice: &'a [V],
+}
 
 // (x = 64, y = 1, z = 1)
 #[spirv(compute(threads(64)))]
 pub fn compute_shader(
     #[spirv(global_invocation_id)] id: UVec3,
-    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] data: &mut [u32],
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] data_0: &mut [u32],
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] data_1: &[f32],
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 2)] data_2: &[i32],
 ) {
+    let test = SliceTest {
+        t_slice: data_0,
+        u_slice: data_1,
+        v_slice: data_2,
+    };
+
     let index = id.x as usize;
-    data[index] = id.y;
+    test.t_slice[index] = id.y;
+    unsafe { debug_printfln!("test.u_slice[%o] = %f", id.x, test.u_slice[index]) }
 }
--- shaders/crates/gpecs_spirv_example_builder/build.rs
+++ shaders/crates/gpecs_spirv_example_builder/build.rs
@@ -1,10 +1,15 @@
 use std::error::Error;
 
-use spirv_builder::{MetadataPrintout, SpirvBuilder};
+use spirv_builder::{MetadataPrintout, ShaderPanicStrategy, SpirvBuilder};
 
 fn main() -> Result<(), Box<dyn Error>> {
     SpirvBuilder::new("../gpecs_spirv_example", "spirv-unknown-spv1.3")
         .print_metadata(MetadataPrintout::Full)
+        .extension("SPV_KHR_non_semantic_info")
+        .shader_panic_strategy(ShaderPanicStrategy::DebugPrintfThenExit {
+            print_inputs: true,
+            print_backtrace: true,
+        })
         .build()?;
 
     Ok(())

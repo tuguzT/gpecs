3a040285d19c810d725000d7f36c5429320e333b
tuguzT <timurka.tugushev@gmail.com>
2024-11-24 22:59:19 +0300

    Loop issue can be partially handled with `unroll`

--- shaders/crates/gpecs_spirv_example/src/lib.rs
+++ shaders/crates/gpecs_spirv_example/src/lib.rs
@@ -1,22 +1,26 @@
 #![cfg_attr(target_arch = "spirv", no_std)]
 #![allow(unexpected_cfgs)] // to not warn about `spirv` target arch
-#![deny(warnings)] // to see warnings from `spirv-builder` builds
 #![forbid(unsafe_op_in_unsafe_fn)]
 #![feature(asm_experimental_arch)]
 
 use spirv_std::{arch::IndexUnchecked, glam::UVec3, spirv};
+use static_assertions as sa;
+use unroll::unroll_for_loops;
 
-// (x = 64, y = 1, z = 1)
+const WORKGROUP_SIZE: usize = 256;
+
+#[allow(unused)] // unroll does not support const variables, so integer literal is used instead
+const ITER_COUNT: usize = WORKGROUP_SIZE / 32;
+sa::const_assert_eq!(ITER_COUNT, 8);
+
 #[spirv(compute(threads(64)))]
+#[unroll_for_loops]
 pub fn compute_shader(
     #[spirv(global_invocation_id)] id: UVec3,
     #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] data: &mut [u32],
 ) {
-    let mut index = 0;
-    while index < 5 {
+    for index in 0..8 {
         let item = unsafe { data.index_unchecked_mut(index) };
         *item = id.x;
-
-        index += 1;
     }
 }

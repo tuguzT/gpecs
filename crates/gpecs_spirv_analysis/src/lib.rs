165965103f20585ae029269e27c11297ccd6835f
tuguzT <timurka.tugushev@gmail.com>
2024-12-04 15:27:37 +0300

    Add `rspirv` dependency for (hopefully) future SPIR-V analysis

---
+++ crates/gpecs_spirv_analysis/src/lib.rs
@@ -0,0 +1,6 @@
+//! Nothing too special, too =)
+
+#![warn(clippy::all)]
+// TODO `#![warn(missing_docs)]` after implementation & tests
+#![forbid(unsafe_code)]
+// TODO `#![no_std]` with `alloc` enabled

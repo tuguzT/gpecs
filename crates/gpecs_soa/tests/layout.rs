a2978a30d2d91ab1147d10941d8bbb0cb3d2c0f8
tuguzT <timurka.tugushev@gmail.com>
2025-02-07 22:24:05 +0300

    Add more layout tests

--- crates/gpecs_soa/tests/layout.rs
+++ crates/gpecs_soa/tests/layout.rs
@@ -1,7 +1,45 @@
-use gpecs_soa::vec::SoaVec;
+use gpecs_soa::{
+    prelude::*,
+    slice::{Iter as SoaIter, IterMut as SoaIterMut},
+    vec::IntoIter as SoaIntoIter,
+};
 
 #[test]
-fn null_opt() {
+fn vec_null_opt() {
     type Vec = SoaVec<(u32, u16, u8)>;
+
     assert_eq!(size_of::<Option<Vec>>(), size_of::<Vec>());
+}
+
+#[test]
+fn slice_null_opt() {
+    type Item = (u32, u16, u8);
+    type Slice = SoaSlice<Item>;
+
+    assert_eq!(size_of::<&Slice>(), size_of::<&[Item]>());
+    assert_eq!(size_of::<Option<&Slice>>(), size_of::<&Slice>());
+
+    assert_eq!(size_of::<&mut Slice>(), size_of::<&mut [Item]>());
+    assert_eq!(size_of::<Option<&mut Slice>>(), size_of::<&mut Slice>());
+}
+
+#[test]
+fn iter_null_opt() {
+    type Iter<'a> = SoaIter<'a, (u32, u16, u8)>;
+
+    assert_eq!(size_of::<Option<Iter>>(), size_of::<Iter>());
+}
+
+#[test]
+fn iter_mut_null_opt() {
+    type IterMut<'a> = SoaIterMut<'a, (u32, u16, u8)>;
+
+    assert_eq!(size_of::<Option<IterMut>>(), size_of::<IterMut>());
+}
+
+#[test]
+fn into_iter_null_opt() {
+    type IntoIter = SoaIntoIter<(u32, u16, u8)>;
+
+    assert_eq!(size_of::<Option<IntoIter>>(), size_of::<IntoIter>());
 }

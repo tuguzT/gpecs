17509fddca1be5840b1ba067bdfb0aa7706a6f05
tuguzT <timurka.tugushev@gmail.com>
2025-03-07 13:25:54 +0300

    Add tests for component registry

--- crates/gpecs/src/component.rs
+++ crates/gpecs/src/component.rs
@@ -35,7 +35,20 @@ pub struct ComponentDescriptor {
 
 impl ComponentDescriptor {
     #[inline]
-    pub fn new<T>() -> Self
+    pub fn new(
+        name: impl Into<Cow<'static, str>>,
+        type_id: Option<TypeId>,
+        layout: Layout,
+    ) -> Self {
+        Self {
+            name: name.into(),
+            type_id,
+            layout,
+        }
+    }
+
+    #[inline]
+    pub fn of<T>() -> Self
     where
         T: Component,
     {
@@ -105,6 +118,14 @@ pub struct ComponentRegistry {
 
 impl ComponentRegistry {
     #[inline]
+    pub fn new() -> Self {
+        Self {
+            components: Vec::new(),
+            type_ids: HashMap::new(),
+        }
+    }
+
+    #[inline]
     pub fn register_component<T>(&mut self) -> ComponentId
     where
         T: Component,
@@ -118,7 +139,7 @@ impl ComponentRegistry {
         type_ids
             .entry(type_id)
             .or_insert_with(|| {
-                let descriptor = ComponentDescriptor::new::<T>();
+                let descriptor = ComponentDescriptor::of::<T>();
                 Self::register_inner(components, descriptor)
             })
             .clone()
@@ -175,5 +196,66 @@ impl ComponentRegistry {
     {
         let type_id = TypeId::of::<T>();
         self.component_id_from(type_id)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct Position {
+        _x: f32,
+        _y: f32,
+        _z: f32,
+    }
+
+    impl Component for Position {}
+
+    #[test]
+    fn new() {
+        let components = ComponentRegistry::new();
+        assert_eq!(components.len(), 0);
+    }
+
+    #[test]
+    fn register_type() {
+        let mut components = ComponentRegistry::new();
+        assert_eq!(components.len(), 0);
+        assert_eq!(components.component_id::<Position>(), None);
+
+        let id = components.register_component::<Position>();
+        assert_eq!(components.len(), 1);
+        assert_eq!(id.index(), 0);
+        assert_eq!(components.component_id::<Position>(), Some(id));
+
+        assert_eq!(components.register_component::<Position>(), id);
+
+        let info = components
+            .get_info(id)
+            .expect("info of just registered component should present");
+        assert_eq!(info.id(), id);
+        assert_eq!(info.type_id(), Some(TypeId::of::<Position>()));
+        assert_eq!(info.name(), type_name::<Position>());
+        assert_eq!(info.layout(), Layout::new::<Position>());
+    }
+
+    #[test]
+    fn register_with_descriptor() {
+        let mut components = ComponentRegistry::new();
+        components.register_component::<Position>();
+        assert_eq!(components.len(), 1);
+
+        let descriptor = ComponentDescriptor::new("Mass", None, Layout::new::<f32>());
+        let id = components.register_component_with_descriptor(descriptor);
+        assert_eq!(components.len(), 2);
+        assert_eq!(id.index(), 1);
+
+        let info = components
+            .get_info(id)
+            .expect("info of just registered component should present");
+        assert_eq!(info.id(), id);
+        assert_eq!(info.type_id(), None);
+        assert_eq!(info.name(), "Mass");
+        assert_eq!(info.layout(), Layout::new::<f32>());
     }
 }

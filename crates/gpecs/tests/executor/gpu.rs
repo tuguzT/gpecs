39e9f461d60ac45b2ee761f2ed018fa621d693b0
tuguzT <timurka.tugushev@gmail.com>
2025-04-17 23:15:17 +0300

    Add `wgpu` crate (to implement GPU executor with)

--- crates/gpecs/tests/executor/gpu.rs
+++ crates/gpecs/tests/executor/gpu.rs
@@ -2,8 +2,41 @@ use gpecs::{prelude::*, soa::identity::Identity};
 
 use crate::common::{Mass, Position};
 
+fn init() {
+    let _ = env_logger::builder().is_test(true).try_init();
+}
+
 #[test]
+#[cfg_attr(miri, ignore)]
 fn register_data() {
+    init();
+
+    let instance_desc = wgpu::InstanceDescriptor::default();
+    let instance = wgpu::Instance::new(&instance_desc);
+
+    let adapter_options = wgpu::RequestAdapterOptions::default();
+    let adapter = pollster::block_on(instance.request_adapter(&adapter_options))
+        .expect("failed to create WGPU adapter");
+    println!("Running on {:#?}", adapter.get_info());
+
+    let downlevel_capabilities = adapter.get_downlevel_capabilities();
+    if !downlevel_capabilities
+        .flags
+        .contains(wgpu::DownlevelFlags::COMPUTE_SHADERS)
+    {
+        panic!("adapter does not support compute shaders");
+    }
+
+    let device_desc = wgpu::DeviceDescriptor {
+        label: Some("gpecs test device"),
+        required_features: wgpu::Features::empty(),
+        required_limits: wgpu::Limits::downlevel_defaults(),
+        memory_hints: wgpu::MemoryHints::MemoryUsage,
+    };
+    let (device, _queue) = pollster::block_on(adapter.request_device(&device_desc, None))
+        .expect("failed to create device & queue");
+    println!("Limits of the current device are {:#?}", device.limits());
+
     let mut context = Context::new();
     let mut executor = GpuExecutor::new(&mut context);
 

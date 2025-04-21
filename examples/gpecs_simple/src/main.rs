db0e9ad9ee56ea6865bda5d1af178c61ab4d4447
tuguzT <timurka.tugushev@gmail.com>
2025-04-21 08:44:58 +0300

    Measure performance of already implemented example

--- examples/gpecs_simple/src/main.rs
+++ examples/gpecs_simple/src/main.rs
@@ -1,4 +1,11 @@
-use std::{ffi::c_void, fs, mem::transmute, ptr::null, slice, time::Duration};
+use std::{
+    ffi::c_void,
+    fs,
+    mem::transmute,
+    ptr::null,
+    slice,
+    time::{Duration, Instant},
+};
 
 use glam::Vec3;
 use gpecs::prelude::*;
@@ -6,6 +13,7 @@ use gpecs_simple_types::*;
 use itertools::Itertools;
 use renderdoc::{RenderDoc, V141};
 
+const ITER_COUNT: usize = 10;
 const ENTITY_COUNT: u32 = if cfg!(debug_assertions) {
     2_400
 } else {
@@ -13,12 +21,9 @@ const ENTITY_COUNT: u32 = if cfg!(debug_assertions) {
 };
 
 fn main() {
-    let filter_level = if cfg!(debug_assertions) {
-        log::LevelFilter::Debug
-    } else {
-        log::LevelFilter::Info
-    };
-    env_logger::builder().filter_level(filter_level).init();
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .init();
 
     let mut context = Context::new();
     setup_context(&mut context);
@@ -26,14 +31,20 @@ fn main() {
     let mut executor = CpuExecutor::new(&mut context);
     let check_positions_system = executor.register_system(update_positions);
     let check_masses_system = executor.register_system(update_masses);
-    let check_tags_system = executor.register_system(check_tags);
+    // let check_tags_system = executor.register_system(check_tags);
 
     // Setup order of systems' execution
     executor.add_system(check_positions_system);
-    executor.add_system(check_tags_system);
+    // executor.add_system(check_tags_system);
     executor.add_system(check_masses_system);
 
-    executor.execute();
+    log::info!("Starting to execute systems on CPU...");
+    for i in 0..ITER_COUNT {
+        let start = Instant::now();
+        executor.execute();
+        let duration = start.elapsed();
+        log::info!("CPU system execution {i} took {duration:?}");
+    }
 
     // Return context from the executor
     let mut context = executor.into_context();
@@ -41,7 +52,6 @@ fn main() {
     let (device, queue) = init_wgpu();
 
     let mut renderdoc = init_renderdoc();
-    renderdoc_start_frame_capture(renderdoc.as_mut(), &device);
 
     context.clear();
     setup_context(&mut context);
@@ -51,7 +61,7 @@ fn main() {
     executor
         .register_archetype::<(Position, Mass)>()
         .expect("archetype of `Position` and `Mass` should contain unique component ids");
-    let position_tag_gpu_archetype_id = executor
+    let _position_tag_gpu_archetype_id = executor
         .register_archetype::<(Position, Tag)>()
         .expect("archetype of `Position` and `Tag` should contain unique component ids");
 
@@ -79,111 +89,135 @@ fn main() {
         )
         .expect("GPU system by shader module should be registered");
 
-    let tag_gpu_id = executor.register_component::<Tag>();
+    let _tag_gpu_id = executor.register_component::<Tag>();
 
     executor.add_system(positions_gpu_system_id);
     executor.add_system(mass_gpu_system_id);
 
     // Create download buffer for archetype of `Position` and `Tag`
-    let position_tag_download_buffer = init_wgpu_position_tag_download_buffer(
-        &executor,
-        position_tag_gpu_archetype_id,
-        position_gpu_id,
-        tag_gpu_id,
-    );
-
-    let mut command_encoder = init_wgpu_command_encoder(&device);
-    executor.execute(&mut command_encoder);
-
-    // Push commands to copy data into the download buffer
-    wgpu_copy_into_position_tag_download_buffer(
-        &executor,
-        position_tag_download_buffer.as_ref(),
-        &mut command_encoder,
-        position_tag_gpu_archetype_id,
-        position_gpu_id,
-        tag_gpu_id,
-    );
+    // let position_tag_download_buffer = init_wgpu_position_tag_download_buffer(
+    //     &executor,
+    //     position_tag_gpu_archetype_id,
+    //     position_gpu_id,
+    //     tag_gpu_id,
+    // );
 
     // Create download buffer for results of timestamp queries
-    let timestamp_query_download_buffer = init_wgpu_timestamp_query_download_buffer(&executor);
+    let mut timestamp_query_download_buffer = None;
 
-    // Push commands to copy data into the timestamp query download buffer
-    wgpu_copy_into_timestamp_query_download_buffer(
-        &executor,
-        timestamp_query_download_buffer.as_ref(),
-        &mut command_encoder,
-    );
+    log::info!("Starting to execute systems on GPU...");
+    for i in 0..ITER_COUNT {
+        renderdoc_start_frame_capture(renderdoc.as_mut(), &device);
+        let start = Instant::now();
 
-    let command_buffer = command_encoder.finish();
-    queue.submit([command_buffer]);
+        let mut command_encoder = init_wgpu_command_encoder(&device);
+        executor.execute(&mut command_encoder);
 
-    // Map download buffer to CPU memory
-    let position_tag_download_slice = wgpu_map_whole_buffer(position_tag_download_buffer.as_ref());
+        // Push commands to copy data into the download buffer
+        // wgpu_copy_into_position_tag_download_buffer(
+        //     &executor,
+        //     position_tag_download_buffer.as_ref(),
+        //     &mut command_encoder,
+        //     position_tag_gpu_archetype_id,
+        //     position_gpu_id,
+        //     tag_gpu_id,
+        // );
 
-    // Map timestamp query download buffer to CPU memory
-    let timestamp_query_download_slice =
-        wgpu_map_whole_buffer(timestamp_query_download_buffer.as_ref());
-
-    device.poll(wgpu::Maintain::Wait).panic_on_timeout();
-
-    // Check data inside of the download buffer
-    if let Some(position_tag_download_slice) = position_tag_download_slice {
-        let position_tag_archetype_info = executor
-            .context()
-            .archetypes()
-            .get_archetype_info(position_tag_gpu_archetype_id.into_id())
-            .expect("archetype info should be present");
-        let position_tag_entities = position_tag_archetype_info.storage().entities();
-
-        let position_tag_download_slice_mapped_range =
-            position_tag_download_slice.get_mapped_range();
-        let position_tag_positions: &[Position] = unsafe {
-            slice::from_raw_parts(
-                position_tag_download_slice_mapped_range.as_ptr().cast(),
-                position_tag_entities.len(),
-            )
-        };
-        log::debug!("Positions of {position_tag_gpu_archetype_id:?}:\n{position_tag_positions:#?}");
-
-        itertools::assert_equal(
-            position_tag_entities.iter().map(|entity| Position {
-                data: Vec3 {
-                    x: entity.index() as f32,
-                    y: (entity.index() as f32) / 2.0,
-                    z: -(entity.index() as f32) / 2.0,
-                },
-            }),
-            position_tag_positions.iter().copied(),
-        );
-    }
-
-    // Check data inside of the timestamp query download buffer
-    if let Some((timestamp_query_download_slice, timestamp_query_resources)) =
-        timestamp_query_download_slice.zip(executor.timestamp_query_resources())
-    {
-        let timestamp_query_download_slice_mapped_range =
-            timestamp_query_download_slice.get_mapped_range();
-        let timestamp_query_raw: &[u64] = unsafe {
-            slice::from_raw_parts(
-                timestamp_query_download_slice_mapped_range.as_ptr().cast(),
-                timestamp_query_resources
-                    .count()
-                    .get()
-                    .try_into()
-                    .expect("count of queries should fit into `usize`"),
-            )
-        };
-
-        let timestamp_period_nanos = queue.get_timestamp_period();
-        for (index, (&first, &second)) in timestamp_query_raw.iter().tuple_windows().enumerate() {
-            let nanos = (second - first) as f32 * timestamp_period_nanos;
-            let duration = Duration::from_nanos(nanos as u64);
-            log::info!("Timestamp query {index} duration: {duration:?}");
+        if timestamp_query_download_buffer.is_none() {
+            timestamp_query_download_buffer = init_wgpu_timestamp_query_download_buffer(&executor);
         }
-    }
 
-    renderdoc_end_frame_capture(renderdoc.as_mut(), &device);
+        // Push commands to copy data into the timestamp query download buffer
+        wgpu_copy_into_timestamp_query_download_buffer(
+            &executor,
+            timestamp_query_download_buffer.as_ref(),
+            &mut command_encoder,
+        );
+
+        let command_buffer = command_encoder.finish();
+        queue.submit([command_buffer]);
+
+        // Map download buffer to CPU memory
+        // let position_tag_download_slice =
+        //     wgpu_map_whole_buffer(position_tag_download_buffer.as_ref());
+
+        // Map timestamp query download buffer to CPU memory
+        let timestamp_query_download_slice =
+            wgpu_map_whole_buffer(timestamp_query_download_buffer.as_ref());
+
+        device.poll(wgpu::Maintain::Wait).panic_on_timeout();
+
+        let duration = start.elapsed();
+        log::info!("GPU system execution {i} overall took {duration:?}");
+
+        // Check data inside of the download buffer
+        // if let Some(position_tag_download_slice) = position_tag_download_slice {
+        //     let position_tag_archetype_info = executor
+        //         .context()
+        //         .archetypes()
+        //         .get_archetype_info(position_tag_gpu_archetype_id.into_id())
+        //         .expect("archetype info should be present");
+        //     let position_tag_entities = position_tag_archetype_info.storage().entities();
+
+        //     let position_tag_download_slice_mapped_range =
+        //         position_tag_download_slice.get_mapped_range();
+        //     let position_tag_positions: &[Position] = unsafe {
+        //         slice::from_raw_parts(
+        //             position_tag_download_slice_mapped_range.as_ptr().cast(),
+        //             position_tag_entities.len(),
+        //         )
+        //     };
+        //     log::debug!(
+        //         "Positions of {position_tag_gpu_archetype_id:?}:\n{position_tag_positions:#?}"
+        //     );
+
+        //     itertools::assert_equal(
+        //         position_tag_entities.iter().map(|entity| Position {
+        //             data: Vec3 {
+        //                 x: entity.index() as f32,
+        //                 y: (entity.index() as f32) / 2.0,
+        //                 z: -(entity.index() as f32) / 2.0,
+        //             },
+        //         }),
+        //         position_tag_positions.iter().copied(),
+        //     );
+        // }
+        // if let Some(position_tag_download_buffer) = position_tag_download_buffer.as_ref() {
+        //     position_tag_download_buffer.unmap();
+        // }
+
+        // Check data inside of the timestamp query download buffer
+        if let Some((timestamp_query_download_slice, timestamp_query_resources)) =
+            timestamp_query_download_slice.zip(executor.timestamp_query_resources())
+        {
+            let timestamp_query_download_slice_mapped_range =
+                timestamp_query_download_slice.get_mapped_range();
+            let timestamp_query_raw: &[u64] = unsafe {
+                slice::from_raw_parts(
+                    timestamp_query_download_slice_mapped_range.as_ptr().cast(),
+                    timestamp_query_resources
+                        .count()
+                        .get()
+                        .try_into()
+                        .expect("count of queries should fit into `usize`"),
+                )
+            };
+            log::info!("Timestamp query raw data: {timestamp_query_raw:?}");
+
+            let timestamp_period_nanos = queue.get_timestamp_period();
+            for (index, (&first, &second)) in timestamp_query_raw.iter().tuple_windows().enumerate()
+            {
+                let nanos = (second - first) as f32 * timestamp_period_nanos;
+                let duration = Duration::from_nanos(nanos as u64);
+                log::info!("Timestamp query {index} duration: {duration:?}");
+            }
+        }
+        if let Some(timestamp_query_download_buffer) = timestamp_query_download_buffer.as_ref() {
+            timestamp_query_download_buffer.unmap();
+        }
+
+        renderdoc_end_frame_capture(renderdoc.as_mut(), &device);
+    }
 }
 
 fn setup_context(context: &mut Context) {
@@ -220,16 +254,17 @@ fn setup_context(context: &mut Context) {
 }
 
 fn update_positions(positions: BundlesMut<(Position,)>) {
-    log::info!("Hello from the CPU system working with positions!");
+    // log::info!("Hello from the CPU system working with positions!");
 
-    let mut positions_count = 0;
+    // let mut positions_count = 0;
+    let start = Instant::now();
     for (entity, (position,)) in positions {
         assert!(matches!(entity.index() % 3, 0 | 2));
-        assert_eq!(position.data.x, entity.index() as f32);
-        assert_eq!(position.data.y, -(entity.index() as f32));
-        assert_eq!(position.data.z, 0.0);
+        // assert_eq!(position.data.x, entity.index() as f32);
+        // assert_eq!(position.data.y, -(entity.index() as f32));
+        // assert_eq!(position.data.z, 0.0);
 
-        log::debug!("{entity} has position of {}", position.data);
+        // log::debug!("{entity} has position of {}", position.data);
         position.data = Vec3 {
             x: entity.index() as f32,
             y: (entity.index() as f32) / 2.0,
@@ -237,43 +272,51 @@ fn update_positions(positions: BundlesMut<(Position,)>) {
         };
         log::debug!("{entity} position have been updated to {}", position.data);
 
-        positions_count += 1;
+        // positions_count += 1;
     }
-    assert_eq!(positions_count, ENTITY_COUNT / 3 * 2);
+    // assert_eq!(positions_count, ENTITY_COUNT / 3 * 2);
+    let duration = start.elapsed();
+    log::info!("CPU system working with positions took {duration:?}");
 }
 
 fn update_masses(context: &mut Context) {
-    log::info!("Hello from the CPU system working with masses!");
+    // log::info!("Hello from the CPU system working with masses!");
 
-    let mut masses_count = 0;
+    // let mut masses_count = 0;
+    let start = Instant::now();
     let masses = context
         .bundles_mut::<(Mass,)>()
         .expect("archetype of `Mass` should exist");
     for (entity, (mass,)) in masses {
         assert!(matches!(entity.index() % 3, 1 | 2));
-        assert_eq!(mass.value, entity.index());
+        // assert_eq!(mass.value, entity.index());
 
-        log::debug!("{entity} has mass of {}", mass.value);
-        mass.value += entity.index();
+        // log::debug!("{entity} has mass of {}", mass.value);
+        mass.value = entity.index();
         log::debug!("{entity} mass have been updated to {}", mass.value);
 
-        masses_count += 1;
+        // masses_count += 1;
     }
-    assert_eq!(masses_count, ENTITY_COUNT / 3 * 2);
+    // assert_eq!(masses_count, ENTITY_COUNT / 3 * 2);
+    let duration = start.elapsed();
+    log::info!("CPU system working with masses took {duration:?}");
 }
 
-fn check_tags(tags: Bundles<(Tag,)>) {
-    log::info!("Hello from the CPU system working with tags!");
+fn _check_tags(tags: Bundles<(Tag,)>) {
+    // log::info!("Hello from the CPU system working with tags!");
 
-    let mut tags_count = 0;
+    // let mut tags_count = 0;
+    let start = Instant::now();
     for (entity, (tag,)) in tags {
         assert!(matches!(entity.index() % 3, 0 | 1));
-        assert_eq!(tag, &Tag);
+        // assert_eq!(tag, &Tag);
 
         log::debug!("{entity} has {tag:?}");
-        tags_count += 1;
+        // tags_count += 1;
     }
-    assert_eq!(tags_count, ENTITY_COUNT / 3 * 2);
+    // assert_eq!(tags_count, ENTITY_COUNT / 3 * 2);
+    let duration = start.elapsed();
+    log::info!("CPU system working with tags took {duration:?}");
 }
 
 fn init_wgpu() -> (wgpu::Device, wgpu::Queue) {
@@ -342,7 +385,7 @@ fn init_wgpu_command_encoder(device: &wgpu::Device) -> wgpu::CommandEncoder {
     device.create_command_encoder(&command_encoder_desc)
 }
 
-fn init_wgpu_position_tag_download_buffer(
+fn _init_wgpu_position_tag_download_buffer(
     executor: &GpuExecutor,
     position_tag_gpu_archetype_id: GpuArchetypeId,
     position_gpu_id: GpuComponentId,
@@ -401,7 +444,7 @@ fn init_wgpu_timestamp_query_download_buffer(executor: &GpuExecutor) -> Option<w
         .into()
 }
 
-fn wgpu_copy_into_position_tag_download_buffer(
+fn _wgpu_copy_into_position_tag_download_buffer(
     executor: &GpuExecutor,
     position_tag_download_buffer: Option<&wgpu::Buffer>,
     command_encoder: &mut wgpu::CommandEncoder,

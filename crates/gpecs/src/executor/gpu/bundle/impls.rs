929bcec784189c346e18668af6b2d996f7ffb1b9
tuguzT <timurka.tugushev@gmail.com>
2026-02-28 20:01:58 +0300

    Rename GPU bundle trait items, add documentation

--- crates/gpecs/src/executor/gpu/bundle/impls.rs
+++ crates/gpecs/src/executor/gpu/bundle/impls.rs
@@ -13,26 +13,26 @@ unsafe impl<T> GpuBundle for Identity<T>
 where
     T: GpuComponent,
 {
-    type MaybeGpuComponentIds = [Option<GpuComponentId>; 1];
+    type GetGpuComponents = [Option<GpuComponentId>; 1];
 
     #[inline]
     fn get_gpu_components(
         components: &ComponentRegistry,
         gpu_components: &GpuComponentRegistry,
-    ) -> Self::MaybeGpuComponentIds {
+    ) -> Self::GetGpuComponents {
         let component_id = components
             .component_id::<T>()
             .and_then(|id| gpu_components.map_component_id(id));
         [component_id]
     }
 
-    type GpuComponentIds = [GpuComponentId; 1];
+    type RegisterGpuComponents = [GpuComponentId; 1];
 
     #[inline]
     fn register_gpu_components(
         components: &mut ComponentRegistry,
         gpu_components: &mut GpuComponentRegistry,
-    ) -> Self::GpuComponentIds {
+    ) -> Self::RegisterGpuComponents {
         let component_id = gpu_components.register_component::<T>(components);
         [component_id]
     }
@@ -44,13 +44,13 @@ macro_rules! gpu_bundle_tuple_impl {
         where
             $($types: GpuComponent,)*
         {
-            type MaybeGpuComponentIds = [Option<GpuComponentId>; count_idents!($($types,)*)];
+            type GetGpuComponents = [Option<GpuComponentId>; count_idents!($($types,)*)];
 
             #[inline]
             fn get_gpu_components(
                 components: &ComponentRegistry,
                 gpu_components: &GpuComponentRegistry,
-            ) -> Self::MaybeGpuComponentIds {
+            ) -> Self::GetGpuComponents {
                 let permutation = Self::Context::PERMUTATION;
 
                 let component_ids = [$(
@@ -62,13 +62,13 @@ macro_rules! gpu_bundle_tuple_impl {
                 component_ids
             }
 
-            type GpuComponentIds = [GpuComponentId; count_idents!($($types,)*)];
+            type RegisterGpuComponents = [GpuComponentId; count_idents!($($types,)*)];
 
             #[inline]
             fn register_gpu_components(
                 components: &mut ComponentRegistry,
                 gpu_components: &mut GpuComponentRegistry,
-            ) -> Self::GpuComponentIds {
+            ) -> Self::RegisterGpuComponents {
                 let permutation = Self::Context::PERMUTATION;
 
                 let component_ids = [$(gpu_components.register_component::<$types>(components),)*];
--- crates/gpecs/src/executor/gpu/bundle/mod.rs
+++ crates/gpecs/src/executor/gpu/bundle/mod.rs
@@ -6,22 +6,36 @@ use crate::{
 
 mod impls;
 
+/// Non-empty collection of [GPU components](crate::executor::gpu::component::GpuComponent).
+///
+/// # Safety
+///
+/// Order of component identifiers defined by
+/// [`GetGpuComponents`](GpuBundle::GetGpuComponents) or [`RegisterGpuComponents`](GpuBundle::RegisterGpuComponents) assotiated types
+/// should be the same as the order of corresponding [descriptors](crate::soa::field::FieldDescriptors::Output).
 pub unsafe trait GpuBundle: Bundle + Copy + Send + Sync {
-    /// Order of component identifiers should be the same as
-    /// the order of corresponding [descriptors](crate::soa::field::FieldDescriptors::Output).
-    type MaybeGpuComponentIds: IntoIterator<Item = Option<GpuComponentId>>;
+    /// Non-empty collection of all already registered GPU components of this bundle.
+    ///
+    /// If some component was not registered yet,
+    /// [`None`] should be returned by its iterator.
+    type GetGpuComponents: IntoIterator<Item = Option<GpuComponentId>>;
 
+    /// Retrieves identifiers of all already registered GPU components of this bundle.
     fn get_gpu_components(
         components: &ComponentRegistry,
         gpu_components: &GpuComponentRegistry,
-    ) -> Self::MaybeGpuComponentIds;
+    ) -> Self::GetGpuComponents;
 
-    /// Order of component identifiers should be the same as
-    /// the order of corresponding [descriptors](crate::soa::field::FieldDescriptors::Output).
-    type GpuComponentIds: IntoIterator<Item = GpuComponentId>;
+    /// Non-empty collection of all GPU components of this bundle.
+    ///
+    /// If some component was not registered yet,
+    /// it should be registered by this method and its identifier should be returned by its iterator.
+    type RegisterGpuComponents: IntoIterator<Item = GpuComponentId>;
 
+    /// Registers all GPU components of this bundle inside of provided registry
+    /// and returns their identifiers.
     fn register_gpu_components(
         components: &mut ComponentRegistry,
         gpu_components: &mut GpuComponentRegistry,
-    ) -> Self::GpuComponentIds;
+    ) -> Self::RegisterGpuComponents;
 }

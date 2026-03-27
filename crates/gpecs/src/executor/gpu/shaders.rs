56a056cf398fc07aaf4d217548902a9f34e94086
tuguzT <timurka.tugushev@gmail.com>
2026-03-27 14:33:45 +0300

    Add compiletest for the main compute shaders crate

--- crates/gpecs/src/executor/gpu/shaders.rs
+++ crates/gpecs/src/executor/gpu/shaders.rs
@@ -1,3 +1,57 @@
-const _SHADER_PATH: &str = env!("gpecs_shaders.spv");
+#[cfg(test)]
+mod tests {
+    use std::fs;
 
-// TODO check SPIR-V shader entry points
+    fn init_wgpu() -> (wgpu::Device, wgpu::Queue) {
+        let instance_desc = wgpu::InstanceDescriptor::new_without_display_handle();
+        let instance = wgpu::Instance::new(instance_desc);
+
+        let adapter_options = wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            ..Default::default()
+        };
+        let adapter = pollster::block_on(instance.request_adapter(&adapter_options))
+            .expect("failed to create adapter");
+
+        let adapter_limits = adapter.limits();
+        let adapter_downlevel_capabilities = adapter.get_downlevel_capabilities();
+
+        assert!(
+            adapter_downlevel_capabilities
+                .flags
+                .contains(wgpu::DownlevelFlags::COMPUTE_SHADERS),
+            "adapter does not support compute shaders, which are required",
+        );
+
+        let device_desc = wgpu::DeviceDescriptor {
+            label: Some("`gpecs` integration test device"),
+            required_features: wgpu::Features::default(),
+            experimental_features: wgpu::ExperimentalFeatures::disabled(),
+            required_limits: adapter_limits,
+            memory_hints: wgpu::MemoryHints::Performance,
+            trace: wgpu::Trace::Off,
+        };
+        let (device, queue) = pollster::block_on(adapter.request_device(&device_desc))
+            .expect("failed to create device & queue");
+
+        (device, queue)
+    }
+
+    #[test]
+    #[ignore] // TODO: remove `PhantomData<ptr::GpuSliceItemPtr<*mut [core::mem::MaybeUninit<u32>]>>` from erased SoA mut pointers iterator
+    fn spirv_to_shader_module() {
+        const SHADER_PATH: &str = env!("gpecs_shaders.spv");
+
+        let (device, _) = init_wgpu();
+        let data = fs::read(SHADER_PATH).expect("SPIR-V shader file should exist");
+
+        let shader_desc = wgpu::ShaderModuleDescriptor {
+            label: Some("`gpecs` shader"),
+            source: wgpu::util::make_spirv(&data),
+        };
+        let shader_module = device.create_shader_module(shader_desc);
+
+        let shader_compilation_info = pollster::block_on(shader_module.get_compilation_info());
+        println!("Shader compilation info: {shader_compilation_info:#?}");
+    }
+}

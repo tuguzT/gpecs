dec10b04f6a0e0ce72327299b7826ebbe333d12f
tuguzT <timurka.tugushev@gmail.com>
2026-04-24 17:49:27 +0300

    Refactor download buffer type state

--- crates/gpecs/src/executor/gpu/cache/transfer.rs
+++ crates/gpecs/src/executor/gpu/cache/transfer.rs
@@ -1,7 +1,7 @@
 use bytemuck::must_cast_slice_mut;
 use wgpu::{
-    Buffer, BufferAsyncError, BufferDescriptor, BufferSlice, BufferUsages, CommandEncoder, Device,
-    MapMode, WasmNotSend,
+    Buffer, BufferAsyncError, BufferDescriptor, BufferSize, BufferSlice, BufferUsages,
+    CommandEncoder, Device, MapMode, WasmNotSend,
 };
 
 use crate::{
@@ -45,20 +45,26 @@ impl TransferCache {
                 continue;
             };
 
-            let archetype_cache = archetypes.entry(archetype_id).or_default();
-            archetype_cache
-                .entities
-                .copy_from_buffer(device, command_encoder, unsafe { entities.as_slice() });
+            let source = unsafe { entities.as_slice() };
+            let archetype_cache = archetypes
+                .entry(archetype_id)
+                .and_modify(|c| c.entities.copy_from_slice(device, command_encoder, source))
+                .or_insert_with(|| {
+                    let entities = DownloadBuffer::from_slice(device, command_encoder, source);
+                    ArchetypeCache::new(entities)
+                });
 
             for (component_id, components) in storage_slices.components {
                 let Some(components) = components else {
                     continue;
                 };
 
-                let download_components =
-                    archetype_cache.components.entry(component_id).or_default();
-                download_components
-                    .copy_from_buffer(device, command_encoder, unsafe { components.as_slice() });
+                let source = unsafe { components.as_slice() };
+                archetype_cache
+                    .components
+                    .entry(component_id)
+                    .and_modify(|b| b.copy_from_slice(device, command_encoder, source))
+                    .or_insert_with(|| DownloadBuffer::from_slice(device, command_encoder, source));
             }
         }
     }
@@ -94,68 +100,85 @@ impl TransferCache {
             let storage = storage.into_meta();
             let (entities, mut bundles, _) = unsafe { storage.as_mut_view().into_mut_slices() };
 
-            let mapped_entities = archetype_cache.entities.get_buffer();
-            let Some(mapped_entities) = mapped_entities else {
-                unreachable!("entities of {archetype_id} should be mapped");
-            };
-            must_cast_slice_mut(entities).copy_from_slice(&mapped_entities.get_mapped_range(..));
-            mapped_entities.unmap();
+            let mapped_entities = archetype_cache.entities.as_slice();
+            must_cast_slice_mut(entities).copy_from_slice(&mapped_entities.get_mapped_range());
+            mapped_entities.buffer().unmap();
 
             for (&component_id, components) in &archetype_cache.components {
-                let Some(mapped_components) = components.get_buffer() else {
-                    unreachable!("{component_id} of {archetype_id} should be mapped");
-                };
+                let mapped_components = components.as_slice();
 
                 let Some(mut components) = bundles.get_mut(component_id.into()) else {
                     continue;
                 };
                 let components = unsafe { components.as_mut_buffer() };
 
-                components.write_copy_of_slice(&mapped_components.get_mapped_range(..));
-                mapped_components.unmap();
+                components.write_copy_of_slice(&mapped_components.get_mapped_range());
+                mapped_components.buffer().unmap();
             }
         }
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct ArchetypeCache {
     entities: DownloadBuffer,
     components: IndexMap<GpuComponentId, DownloadBuffer>,
 }
 
-#[derive(Debug, Default)]
+impl ArchetypeCache {
+    pub fn new(entities: DownloadBuffer) -> Self {
+        Self {
+            entities,
+            components: IndexMap::default(),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct DownloadBuffer {
-    buffer: Option<Buffer>,
+    buffer: Buffer,
+    init_size: BufferSize,
 }
 
 impl DownloadBuffer {
     #[inline]
-    pub fn copy_from_buffer(
+    pub fn from_slice(
+        device: &Device,
+        command_encoder: &mut CommandEncoder,
+        source: BufferSlice<'_>,
+    ) -> Self {
+        let init_size = source.size();
+
+        let size = init_size.get();
+        let desc = BufferDescriptor {
+            label: Some("`gpecs` transfer cache download buffer"),
+            size,
+            usage: BufferUsages::MAP_READ | BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        };
+        let buffer = device.create_buffer(&desc);
+
+        command_encoder.copy_buffer_to_buffer(source.buffer(), source.offset(), &buffer, 0, size);
+        Self { buffer, init_size }
+    }
+
+    #[inline]
+    pub fn copy_from_slice(
         &mut self,
         device: &Device,
-        encoder: &mut CommandEncoder,
+        command_encoder: &mut CommandEncoder,
         source: BufferSlice<'_>,
     ) {
-        let Self { buffer } = self;
+        let Self { buffer, init_size } = self;
 
         let size = source.size().get();
-        let new_buffer = || {
-            let desc = BufferDescriptor {
-                label: Some("`gpecs` cache download buffer"),
-                size,
-                usage: BufferUsages::MAP_READ | BufferUsages::COPY_DST,
-                mapped_at_creation: false,
-            };
-            device.create_buffer(&desc)
-        };
-        let buffer = buffer.get_or_insert_with(new_buffer);
-
         if buffer.size() < size {
-            *buffer = new_buffer();
+            *self = Self::from_slice(device, command_encoder, source);
+            return;
         }
 
-        encoder.copy_buffer_to_buffer(source.buffer(), source.offset(), buffer, 0, size);
+        *init_size = source.size();
+        command_encoder.copy_buffer_to_buffer(source.buffer(), source.offset(), buffer, 0, size);
     }
 
     #[inline]
@@ -163,15 +186,12 @@ impl DownloadBuffer {
     where
         F: FnOnce(Result<(), BufferAsyncError>) + WasmNotSend + 'static,
     {
-        let Self { buffer } = self;
-        if let Some(buffer) = buffer {
-            buffer.map_async(MapMode::Read, .., callback);
-        }
+        self.as_slice().map_async(MapMode::Read, callback);
     }
 
     #[inline]
-    pub fn get_buffer(&self) -> Option<&Buffer> {
-        let Self { buffer } = self;
-        buffer.as_ref()
+    pub fn as_slice(&self) -> BufferSlice<'_> {
+        let Self { buffer, init_size } = self;
+        buffer.slice(..init_size.get())
     }
 }

59d63504e21fe5e7591d90e31a950859e67ba3f7
tuguzT <timurka.tugushev@gmail.com>
2026-05-10 18:51:38 +0300

    Move CPU system registration of simple example into a function

--- examples/gpecs_simple/src/cpu.rs
+++ examples/gpecs_simple/src/cpu.rs
@@ -14,14 +14,7 @@ pub fn run(context: &mut Context, entity_count: u32, repeat_count: Option<usize>
     setup::setup(context, entity_count);
 
     let mut executor = CpuExecutor::new(context);
-    let check_positions_system = executor.register_system(update_positions);
-    let check_masses_system = executor.register_system(update_masses);
-    // let check_tags_system = executor.register_system(check_tags);
-
-    // Setup order of systems' execution
-    executor.add_system(check_positions_system);
-    // executor.add_system(check_tags_system);
-    executor.add_system(check_masses_system);
+    register_cpu_systems(&mut executor);
 
     log::info!("Starting to execute systems on CPU...");
     for i in (0_u128..).maybe_take(repeat_count) {
@@ -36,74 +29,87 @@ pub fn run(context: &mut Context, entity_count: u32, repeat_count: Option<usize>
     executor.into_context()
 }
 
-fn update_positions(positions: BundlesMut<(Position,)>) {
-    // log::info!("Hello from the CPU system working with positions!");
+fn register_cpu_systems(executor: &mut CpuExecutor) {
+    let update_positions_system = executor.register_system(|positions: BundlesMut<(Position,)>| {
+        // log::info!("Hello from the CPU system working with positions!");
 
-    let positions = positions.collect_vec().into_par_iter();
-    positions.for_each(|(archetype_id, positions)| {
-        let start = Instant::now();
+        let positions = positions
+            .filter(|(_, bundles)| !bundles.is_empty())
+            .collect_vec()
+            .into_par_iter();
+        positions.for_each(|(archetype_id, positions)| {
+            let start = Instant::now();
 
-        positions.into_iter().for_each(|(entity, (position,))| {
-            assert!(matches!(entity.index() % 3, 0 | 2));
-            // assert_eq!(position.data.x, entity.index() as f32);
-            // assert_eq!(position.data.y, -(entity.index() as f32));
-            // assert_eq!(position.data.z, 0.0);
+            positions.into_iter().for_each(|(entity, (position,))| {
+                assert!(matches!(entity.index() % 3, 0 | 2));
+                // assert_eq!(position.data.x, entity.index() as f32);
+                // assert_eq!(position.data.y, -(entity.index() as f32));
+                // assert_eq!(position.data.z, 0.0);
 
-            // log::debug!("{entity} has position of {}", position.data);
-            position.data = Vec3 {
-                x: entity.index().to_f32().unwrap(),
-                y: entity.index().to_f32().unwrap() / 2.0,
-                z: -entity.index().to_f32().unwrap() / 2.0,
-            };
-            log::debug!("{entity} position have been updated to {}", position.data);
+                // log::debug!("{entity} has position of {}", position.data);
+                position.data = Vec3 {
+                    x: entity.index().to_f32().unwrap(),
+                    y: entity.index().to_f32().unwrap() / 2.0,
+                    z: -entity.index().to_f32().unwrap() / 2.0,
+                };
+                log::debug!("{entity} position have been updated to {}", position.data);
+            });
+
+            let duration = start.elapsed();
+            log::info!("CPU system `update_positions` with {archetype_id} took {duration:?}");
         });
-
-        let duration = start.elapsed();
-        log::info!("CPU system `update_positions` with {archetype_id} took {duration:?}");
     });
-}
 
-fn update_masses(context: &mut Context) {
-    // log::info!("Hello from the CPU system working with masses!");
+    let update_masses_system = executor.register_system(|context: &mut Context| {
+        // log::info!("Hello from the CPU system working with masses!");
 
-    let masses = context
-        .bundles_mut::<(Mass,)>()
-        .expect("archetype of `Mass` should exist")
-        .collect_vec()
-        .into_par_iter();
-    masses.for_each(|(archetype_id, masses)| {
-        let start = Instant::now();
+        let masses = context
+            .bundles_mut::<(Mass,)>()
+            .expect("archetype of `Mass` should exist")
+            .filter(|(_, bundles)| !bundles.is_empty())
+            .collect_vec()
+            .into_par_iter();
+        masses.for_each(|(archetype_id, masses)| {
+            let start = Instant::now();
 
-        masses.into_iter().for_each(|(entity, (mass,))| {
-            assert!(matches!(entity.index() % 3, 1 | 2));
-            // assert_eq!(mass.value, entity.index());
+            masses.into_iter().for_each(|(entity, (mass,))| {
+                assert!(matches!(entity.index() % 3, 1 | 2));
+                // assert_eq!(mass.value, entity.index());
 
-            // log::debug!("{entity} has mass of {}", mass.value);
-            mass.value = entity.index();
-            log::debug!("{entity} mass have been updated to {}", mass.value);
+                // log::debug!("{entity} has mass of {}", mass.value);
+                mass.value = entity.index();
+                log::debug!("{entity} mass have been updated to {}", mass.value);
+            });
+
+            let duration = start.elapsed();
+            log::info!("CPU system `update_masses` with {archetype_id} took {duration:?}");
         });
-
-        let duration = start.elapsed();
-        log::info!("CPU system `update_masses` with {archetype_id} took {duration:?}");
     });
-}
 
-fn _check_tags(tags: Bundles<(Tag,)>) {
-    // log::info!("Hello from the CPU system working with tags!");
+    let _check_tags_system = executor.register_system(|tags: Bundles<(Tag,)>| {
+        // log::info!("Hello from the CPU system working with tags!");
 
-    let tags = tags.collect_vec().into_par_iter();
-    tags.for_each(|(archetype_id, tags)| {
-        let start = Instant::now();
+        let tags = tags
+            .filter(|(_, bundles)| !bundles.is_empty())
+            .collect_vec()
+            .into_par_iter();
+        tags.for_each(|(archetype_id, tags)| {
+            let start = Instant::now();
 
-        tags.into_iter().for_each(|(entity, (tag,))| {
-            assert!(matches!(entity.index() % 3, 0 | 1));
-            assert_eq!(tag, &Tag);
+            tags.into_iter().for_each(|(entity, (tag,))| {
+                assert!(matches!(entity.index() % 3, 0 | 1));
+                assert_eq!(tag, &Tag);
 
-            log::debug!("{entity} has {tag:?}");
-            // tags_count += 1;
+                log::debug!("{entity} has {tag:?}");
+                // tags_count += 1;
+            });
+
+            let duration = start.elapsed();
+            log::info!("CPU system `check_tags` with {archetype_id} took {duration:?}");
         });
-
-        let duration = start.elapsed();
-        log::info!("CPU system `check_tags` with {archetype_id} took {duration:?}");
     });
+
+    executor.add_system(update_positions_system);
+    // executor.add_system(check_tags_system);
+    executor.add_system(update_masses_system);
 }

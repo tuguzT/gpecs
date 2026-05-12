017ee35a8c1f3e0a97b662271b3481146b5b98ec
tuguzT <timurka.tugushev@gmail.com>
2026-05-12 17:19:30 +0300

    Configure iteration count with env file

--- benches/gpecs_ecs_benchmark/benches/ecs_benchmark.rs
+++ benches/gpecs_ecs_benchmark/benches/ecs_benchmark.rs
@@ -1,3 +1,5 @@
+use std::{any, env, str::FromStr};
+
 use gpecs::prelude::*;
 use gpecs_ecs_benchmark::{cpu, gpu};
 
@@ -5,15 +7,25 @@ fn main() {
     dotenvy::dotenv().ok();
     env_logger::builder().init();
 
-    let entity_count = std::env::var("ENTITY_COUNT")
-        .map(|v| v.parse().expect("`ENTITY_COUNT` env should contain `u32`"))
-        .unwrap_or(1_000_000);
-
     let context = &mut Context::new();
+    let entity_count = parse_env("ENTITY_COUNT").unwrap_or(1_000_000);
 
-    let context = cpu::run(context, entity_count, Some(12));
+    let cpu_repeat_count = parse_env("CPU_REPEAT_COUNT");
+    let context = cpu::run(context, entity_count, cpu_repeat_count);
     context.destroy_all();
 
-    let context = gpu::run(context, entity_count, Some(12));
+    let gpu_repeat_count = parse_env("GPU_REPEAT_COUNT");
+    let context = gpu::run(context, entity_count, gpu_repeat_count);
     context.destroy_all();
+}
+
+fn parse_env<T>(key: &str) -> Option<T>
+where
+    T: FromStr,
+{
+    let Ok(value) = env::var(key).ok()?.parse() else {
+        let type_name = any::type_name::<T>();
+        panic!("`{key}` env should be `{type_name}`")
+    };
+    Some(value)
 }

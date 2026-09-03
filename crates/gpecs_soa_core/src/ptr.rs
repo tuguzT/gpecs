0f0244b5aab58899e0ab6e674e134e835b12ef97
tuguzT <timurka.tugushev@gmail.com>
2026-09-03 22:07:36 +0300

    Do not allow to drop result of SoA read before successful write of another value

--- crates/gpecs_soa_core/src/ptr.rs
+++ crates/gpecs_soa_core/src/ptr.rs
@@ -1,16 +1,18 @@
+use core::mem::ManuallyDrop;
+
 use crate::traits::{MutPtrs, RawSoaContext, ReadSoaContext, SoaRead, SoaWrite, WriteSoaContext};
 
 /// Version of [`core::ptr::replace()`] but for [SoA](crate::traits::RawSoa) types.
-pub unsafe fn replace<'a, T, R, W>(context: &'a T::Context, dest: MutPtrs<'a, T>, src: W) -> R
+pub unsafe fn replace<'a, T, R, W>(context: &'a T::Context, dst: MutPtrs<'a, T>, src: W) -> R
 where
     T: SoaRead<'a, R> + SoaWrite<W> + ?Sized,
 {
-    // SAFETY: We read from `dest` but directly write `src` into it afterwards,
-    // such that the old value is not duplicated. Nothing is dropped and
-    // nothing here can panic.
-    unsafe {
-        let result = context.ptrs_read(context.ptrs_cast_const(dest.clone()));
-        context.ptrs_write(dest, src);
-        result
-    }
+    let result = unsafe {
+        let src = context.ptrs_cast_const(dst.clone());
+        context.ptrs_read(src)
+    };
+    let result = ManuallyDrop::new(result);
+
+    unsafe { context.ptrs_write(dst, src) }
+    ManuallyDrop::into_inner(result)
 }
